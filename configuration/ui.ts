3923414378107cd2bd86a22e02c0025924dbd8e2
yuwensun <michelleannasun@gmail.com>
2025-02-19 16:24:05 -0500

    Update ui.ts
    Clear Button Text

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,7 +3,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `[The FIFA World Cup is coming next year!]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Yu Wen @ UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const CLEAR_BUTTON_TEXT: string = `[Change my mind]`;
 export const PAGE_TITLE: string = `[FIFA World Cup 2026]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
