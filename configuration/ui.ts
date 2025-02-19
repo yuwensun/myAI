d441ac892bc14f4e33ce6eeb6ff34fe05d722a00
yuwensun <michelleannasun@gmail.com>
2025-02-19 16:10:46 -0500

    Update ui.ts
    Page Title, Page Description Change

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `[The FIFA World Cup is coming next year!]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Yu Wen @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_TITLE: string = `[FIFA World Cup 2026]`;
+export const PAGE_DESCRIPTION: string = `Chat with ${Soccer Maniac}, ${Yu Wen}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
