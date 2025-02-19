a02837aeefa1b51b490c9154af2b0d94b88c307b
yuwensun <michelleannasun@gmail.com>
2025-02-19 16:39:48 -0500

    Update identity.ts
    Owner's Name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,4 +1,4 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
+export const OWNER_NAME: string = `[Yu Wen]`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `[Soccer Maniac]`;
