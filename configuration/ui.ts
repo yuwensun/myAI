2cf7d475cba0295b0f2a665e4e00c0a5c90d023f
yuwensun <michelleannasun@gmail.com>
2025-02-19 15:40:27 -0500

    Update ui.ts
    Chat Header

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `[Hello]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
