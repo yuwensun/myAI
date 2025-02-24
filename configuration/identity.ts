91a79886ef29ccb283e26ac1b813135c5dda593a
yuwensun <michelleannasun@gmail.com>
2025-02-24 14:33:10 -0500

    Update identity.ts
    Change the tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `[Yu Wen]`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `[Soccer Maniac]`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `[Casual]`;
 export const AI_ROLE: string = `[AI ROLE]`;
